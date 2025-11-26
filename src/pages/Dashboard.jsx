9110383e2a387850fbaa675616b875a68ea2653d
ibrahimshafiq181898 <ibrahimshafiq1298@gmail.com>
2025-11-26 22:16:21 +0100

    newchanges

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledNavbar } from "./Dashboard.styles.js";
+
+const Dashboard = () => {
+  return (
+    <>
+      <StyledNavbar></StyledNavbar>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Dashboard.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledNavbar = styled.div`
+  width: 100vw; /* full viewport width */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 18px 2rem; /* horizontal padding inside navbar */
+  background-color: lightblue;
+  box-sizing: border-box; /* include padding in width */
+`;
